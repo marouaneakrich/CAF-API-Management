81199a2ea45a62abb00201d04be378acd0c065f0
wiissal <wissaloa1@gmail.com>
2025-11-25 22:34:47 +0100

    feat(routes): import team controller functions

--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -1,2 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); //create router object
+//import controller 
+const { getAllTeams, getTeamById, createTeam, updateTeam, deleteTeam} = require ('../controllers/teamController');
+
