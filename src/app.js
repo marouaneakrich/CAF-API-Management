6093cbb4a1944502216833466159781ff6d51b9a
wiissal <wissaloa1@gmail.com>
2025-11-26 11:27:30 +0100

    creating the app.js to test  and sequelize dataBase

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const teamRoutes = require("./routes/teamRoutes");
+const { sequelize } = require('./models');
+// Sync database (create tables automatically)
+sequelize.sync()
+  .then(() => console.log('Database synced!'))
+  .catch(err => console.log('Database sync error:', err));
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/teams", teamRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
