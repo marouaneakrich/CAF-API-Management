2d1995d0b00e52be66e503c8a0828c07a9f221e1
wiissal <wissaloa1@gmail.com>
2025-11-26 18:15:47 +0100

    update GET /api/players with pagination and team info

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -2,7 +2,40 @@ const { Player, Team } = require("../models");
 
 //get all players
 exports.getAllPlayers = async (req, res) => {
-  res.status(200).json({ message: "Get all players" });
+ try{
+  const page = parseInt (req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const offset = (page -1) * limit;
+  const {count, rows} = await Player.findAndCountAll({
+    limit,
+    offset,
+    attributes: ["id", "name", "position", "number", "age", "team_id"],
+    include:[
+      {
+        model: Team,
+        as: 'team',
+        attributes: ["id", "name", "country"],
+      },
+    ],
+  });
+  res.status(200).json({
+    success: true,
+    message: "Players retrieved successfully",
+    data: rows,
+    pagination:{
+      total: count,
+      page,
+      limit,
+      pages: Math.ceil(count / limit),
+    },
+  });
+ } catch (error) {
+  res.status(500).json({
+    success: false,
+    message :"Error retrieving players",
+    error: error.message
+  });
+ }
 };
 
 //get players by team
