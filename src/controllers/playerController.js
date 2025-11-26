ff552f393e68ecb43fc1089c34a06e22b7c6f42b
wiissal <wissaloa1@gmail.com>
2025-11-27 00:04:45 +0100

    get players using team id

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -3,9 +3,9 @@ const { Player, Team } = require("../models");
 //get all players
 exports.getAllPlayers = async (req, res) => {
  try{
-  const page = parseInt (req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
-  const offset = (page -1) * limit;
+  const page = parseInt (req.query.page) || 1; //def page
+  const limit = parseInt(req.query.limit) || 10; //max 10 per page
+  const offset = (page -1) * limit; //skip .... offset formula
   const {count, rows} = await Player.findAndCountAll({
     limit,
     offset,
@@ -26,7 +26,7 @@ exports.getAllPlayers = async (req, res) => {
       total: count,
       page,
       limit,
-      pages: Math.ceil(count / limit),
+      pages: Math.ceil(count / limit), // Calculating the number of pages 
     },
   });
  } catch (error) {
@@ -40,9 +40,50 @@ exports.getAllPlayers = async (req, res) => {
 
 //get players by team
 exports.getPlayersByTeam = async (req, res) => {
-  res.status(200).json({ message: "Get players by team" });
+  try{
+const {teamId} = req.params;
+const page =parseInt(req.query.page) || 1;
+const limit = parseInt(req.query.limit) || 10;
+const offset = (page - 1) *limit;
+ //check if team exist
+ const team = await Team.findByPk(teamId)
+ if(!team) {
+  return res.status(404).json({
+    success: false,
+    message: "Team Not Found",
+  });
+ }
+//get players for this team
+const {count,rows} = await Player.findAndCountAll({
+  where: {team_id: teamId},
+  limit,
+  offset,
+  attributes: ["id" ,"name", "position", "number", "age"],
+  include: [{
+    model: Team,
+    as: 'team',
+    attributes: ["id", "name", "country"],
+  },],
+});
+res.status(200).json({
+  success: true,
+  message:`Players from $(team.name) retrieved successfully`,
+  data: rows,
+  pagination :{
+    total: count,
+    page,
+    limit,
+    pages: Math.ceil(count / limit),
+  },
+});
+  }catch (error ){
+    res.status(500).json({
+     success: false,
+     message: "Error retrieving team players",
+     error: error.message,
+    });
+  }
 };
-
 //get player by id
 exports.getPlayerById = async (res, rep) => {
   res.status(200).json({ message: "Get player by ID" });
