25d1c11f5fb7ab79b3b9811bd7fa5c77318eeb42
wiissal <wissaloa1@gmail.com>
2025-11-26 11:43:49 +0100

    import the controller  and create routes

--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -1,5 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router(); //create router object
-//import controller 
-const { getAllTeams, getTeamById, createTeam, updateTeam, deleteTeam} = require ('../controllers/teamController');
+//import controller
+const {
+  getAllTeams,
+  getTeamById,
+  createTeam,
+  updateTeam,
+  deleteTeam,
+} = require("../controllers/teamController");
+router.get("/", getAllTeams);
+router.get("/:id", getTeamById);
+router.post("/", createTeam);
+router.put("/:id", updateTeam);
+router.delete("/:id",  deleteTeam);
 
+module.exports = router;
