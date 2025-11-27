58dd66680d80bb8b79627d5f8cfbbcf351b44832
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-11-28 00:11:55 +0100

    Add authMiddleware file authentication and authorization

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { authentication } = require('../middlewares/authMiddleware');
 
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/profile', authController.getProfile);
+router.get('/profile', authentication, authController.getProfile);
 
 module.exports = router;
