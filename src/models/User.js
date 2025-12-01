0daafeaaf08967d0773c02551139484dba41be3e
marouane.akrich <82279645+marouaneakrich@users.noreply.github.com>
2025-12-01 07:22:31 +0100

    Fix: User.js & playRoutes.js & teamRoutes.js & package.json

--- src/models/User.js
+++ src/models/User.js
@@ -1,48 +1,53 @@
 const { DataTypes } = require('sequelize');
 const bcrypt = require('bcryptjs');
-const sequelize = require('../config/database');
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      len: [3, 30]
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 30]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('admin', 'user'),
+      defaultValue: 'user'
     }
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  role: {
-    type: DataTypes.ENUM('admin', 'user'),
-    defaultValue: 'user'
-  }
-}, {
-  timestamps: true,
-  tableName: 'users'
-});
+  }, {
+    timestamps: true,
+    tableName: 'users'
+  });
 
-User.beforeCreate(async (user) => {
-  user.password = await bcrypt.hash(user.password, 10);
-});
+  User.beforeCreate(async (user) => {
+    user.password = await bcrypt.hash(user.password, 10);
+  });
 
-User.prototype.comparePassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
+  User.prototype.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+  User.associate = (models) => {
+    // associations can be defined here if needed
+  };
+
+  return User;
 };
-
-module.exports = User;
